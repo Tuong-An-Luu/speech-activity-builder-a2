dc423ec13c11c345779f361aea66c2fd047352fb
Tuong An Luu <304440957+Tuong-An-Luu@users.noreply.github.com>
2026-08-07 21:23:47 +1000

    Add About tutorial video and references

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -46,6 +46,39 @@ export default function AboutPage() {
         <source src="/how-to-use.mp4" type="video/mp4" />
         Your browser does not support embedded video.
       </video>
+
+      <h2>References</h2>
+
+      <ol>
+        <li>
+          React. (n.d.). React documentation.
+          https://react.dev/
+        </li>
+
+        <li>
+          Vercel. (n.d.). Next.js documentation.
+          https://nextjs.org/docs
+        </li>
+
+        <li>
+          World Wide Web Consortium. (2018).
+          Web Content Accessibility Guidelines (WCAG) 2.1.
+          https://www.w3.org/TR/WCAG21/
+        </li>
+
+        <li>
+          Nielsen, J. (1994).
+          Usability engineering.
+          Morgan Kaufmann.
+        </li>
+
+        <li>
+          International Phonetic Association. (1999).
+          Handbook of the International Phonetic Association:
+          A guide to the use of the International Phonetic Alphabet.
+          Cambridge University Press.
+        </li>
+      </ol>
     </section>
   );
 }
